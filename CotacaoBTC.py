91b8912bc3ca8a459e5c2faf9b5cae52604989b4
Jonas Gomes <jj.ti@hotmail.com>
2017-08-15 21:49:49 -0300

    Update CotacaoBTC.py

--- CotacaoBTC.py
+++ CotacaoBTC.py
@@ -1,10 +1,13 @@
 import bs4
 import requests
+import ChavesAPI
+from datetime import datetime
 
 
 def btc():
     site = "https://br.investing.com/currencies/btc-brl"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/50.0.2661.102 Safari/537.36'}
     result = requests.get(site, headers=headers)
 
     soup = bs4.BeautifulSoup(result.content.decode(), "html.parser")
@@ -15,22 +18,33 @@ def btc():
 
     return float(valorBTC[0:5])
 
+
+def main(alta, baixa):
+    while True:
+        try:
+            cotacao = btc()
+
+            if cotacao > (alta + 500):
+                print("Cotacao maxima anterior:", alta)
+                alta = cotacao
+                print("Bitcoin subindo R$", alta)
+
+                ChavesAPI.twitter1.notify("Cotacao aumentou as " + data + " ")
+
+            if cotacao < (baixa - 500):
+                print("Cotacao baixa anterior:", baixa)
+                baixa = cotacao
+                print("Bitcoin descendo R$", baixa)
+
+                ChavesAPI.twitter1.notify("Cotacao diminuiu as " + data + " ")
+
+        except Exception as e:
+            print(e)
+
+agora = datetime.now()
+data = str(agora.day) + "/" + str(agora.month) + " - " + str(agora.hour) + ":" + str(agora.minute)
+
 maxima = btc()
-baixa = btc()
+minima = btc()
 
-while True:
-    try:
-        cotacao = btc()
-
-        if cotacao > maxima:
-            print("Cotacao maxima anterior:", maxima)
-            maxima = cotacao
-            print("Bitcoin subindo R$", maxima)
-
-        if cotacao < baixa:
-            print("Cotacao baixa anterior:", baixa)
-            baixa = cotacao
-            print("Bitcoin descendo R$", baixa)
-
-    except Exception as e:
-        print(e)
+main(maxima, minima)
